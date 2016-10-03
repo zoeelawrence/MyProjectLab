27bee57e773fd38a917f00b50b5ef2820e5f79c4
zoeelawrence <lawrenzoee@gmail.com>
2016-10-03 15:13:58 +0100

    php test

---
+++ helloworld.php
@@ -0,0 +1,22 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1406425
+ * Date: 03/10/2016
+ * Time: 15:09
+ */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <p>
+        <?php
+            echo "Hello World";
+        ?>
+    </p>
+</body>
+</html>
