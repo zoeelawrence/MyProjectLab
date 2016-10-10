c96141b6c25e9074a9ae4694ea36710fe4fa6d31
zoeelawrence <lawrenzoee@gmail.com>
2016-10-10 15:31:38 +0100

    php test

---
+++ dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_SERVER', 'servername');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'database');
+
+$db =  mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+/**
+ * Created by PhpStorm.
+ * User: 1406425
+ * Date: 10/10/2016
+ * Time: 15:27
+ */
