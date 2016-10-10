c5f56f91542f63f305222880f9d96e3ba2f6d554
zoeelawrence <lawrenzoee@gmail.com>
2016-10-10 15:34:47 +0100

    php test

--- dbconnect.php
+++ dbconnect.php
@@ -6,6 +6,10 @@ define('DB_PASSWORD', 'password');
 define('DB_DATABASE', 'database');
 
 $db =  mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if($db-> connect_errno){
+    die('Connect failed['.$db->connect_error.']');
+}
 /**
  * Created by PhpStorm.
  * User: 1406425
